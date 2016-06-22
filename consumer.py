f5d584d2c1f57a8281d081a8a8880b69a846401e
Simon Fojtu <simon.fojtu@gmail.com>
2016-06-22 22:48:17 +0200

    Fixed circle detector

--- consumer.py
+++ consumer.py
@@ -93,14 +93,19 @@ def drawCorners(img):
     
 def drawCircles(image):
     gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (9, 9), 2, 2)
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 2, 1)
+    gray = cv2.GaussianBlur(gray, (13, 13), 6)
 
-    img = image.copy()
-    for circle in circles[0]:
-        c = (circle[0], circle[1])
-        r = circle[2]
-        cv2.circle(img, c, r, (0, 255, 0), 1)
+    circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,20,
+                                        param1=50,param2=30,minRadius=9,maxRadius=0)
+
+    circles = np.uint16(np.around(circles))
+
+    img = gray.copy()
+    if not circles is None:
+        for circle in circles[0]:
+            c = (circle[0], circle[1])
+            r = circle[2]
+            cv2.circle(img, c, r, 255, 4)
     cv2.imshow("circles", img)
 
 
