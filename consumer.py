c92dc48263a243ecc5a6a5d9b4cbdfb29d76aeba
Simon Fojtu <simon.fojtu@gmail.com>
2016-06-24 17:01:03 +0200

    Fixed exception when no circle is detected

--- consumer.py
+++ consumer.py
@@ -98,10 +98,10 @@ def drawCircles(image):
     circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,20,
                                         param1=50,param2=30,minRadius=9,maxRadius=0)
 
-    circles = np.uint16(np.around(circles))
 
     img = gray.copy()
     if not circles is None:
+        circles = np.uint16(np.around(circles))
         for circle in circles[0]:
             c = (circle[0], circle[1])
             r = circle[2]
